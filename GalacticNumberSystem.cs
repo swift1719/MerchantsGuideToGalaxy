be606a81c00b5e07b731490719a8bdb458c2adb3
swift1719 <ayushpawar1501@gmail.com>
2022-08-27 16:53:34 +0530

    created galactic number system conversion

---
+++ GalacticNumberSystem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantsGuideToGalaxy
+{
+    internal class GalacticNumberSystem
+    {
+        private Dictionary<string, RomanDigit> _aliases = new Dictionary<string, RomanDigit>();
+
+        public void SetAlias(string alias, RomanDigit digit)
+        {
+            _aliases[alias] = digit;
+        }
+
+        public RomanDigit GetAlias(string alias)
+        {
+            return _aliases[alias];
+        }
+
+        public int GetDecimalValue(string number)
+        {
+            int result = 0;
+
+            var digits = number.Split('.').Select(GetAlias).ToArray();
+
+            if (digits.Length == 1)
+            {
+                return (int)digits.Single();
+            }
+
+            var index = 0;
+            var sum = 0;
+
+
+            while (index < digits.Length)
+            {
+                sum = 0;
+                var current = (int)digits[index];
+                var next = index < digits.Length - 1 ? (int)digits[index + 1] : 0;
+
+                if(next > current)
+                {
+                    sum = next - current;
+                    index += 2;
+                }
+                else
+                {
+                    sum = current;
+                    index++;
+                }
+                result += sum;
+            }
+
+            return result;
+        }
+    }
+}
