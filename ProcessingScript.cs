da452d291bcaf23d9cfed50728f3c79ad4155c3c
swift1719 <ayushpawar1501@gmail.com>
2022-08-27 19:57:29 +0530

    scripting input output

---
+++ ProcessingScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantsGuideToGalaxy
+{
+    public class ProcessingScript
+    {
+        public List<string> PlayScript(string[] dialogues)
+        {
+            var merchant = new Merchant();
+            var responses = new List<string>();
+            foreach (var dialogue in dialogues)
+            {
+                var isQuestion = dialogue.EndsWith("?", StringComparison.Ordinal);
+                if (isQuestion == true)
+                    responses.Add(merchant.RespondToQuery(dialogue).Text);
+                else
+                    merchant.CollectInformation(dialogue);
+            }
+            return responses;
+        }
+    }
+}
