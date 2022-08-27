50d2ec279a388c8f3289c51964d24fdd38ff75d8
swift1719 <ayushpawar1501@gmail.com>
2022-08-27 16:36:06 +0530

    Created RomanNumberSystem map

---
+++ RomanNumberSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantsGuideToGalaxy
+{
+    public enum RomanDigit
+    {
+        I=1,
+        V=5,
+        X=10,
+        L=50,
+        C=100,
+        D=500,
+        M=1000
+    }
+}
