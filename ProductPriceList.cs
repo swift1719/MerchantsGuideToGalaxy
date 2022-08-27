05cdefb070cc5efaf5b8ee545762cebd5f1ed061
swift1719 <ayushpawar1501@gmail.com>
2022-08-27 17:01:35 +0530

    create a Product Pricing List

---
+++ ProductPriceList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantsGuideToGalaxy
+{
+    public class ProductPriceList
+    {
+        private Dictionary<string, Product> _products  = new Dictionary<string, Product>();
+
+        public void AddProduct(Product product)
+        {
+            _products[product.Name] = product;
+        }
+
+        public float GetProductUnitPrice(string productName)
+        {
+            return _products[productName].Price;
+        }
+    }
+}
