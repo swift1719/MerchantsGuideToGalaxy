a5337784410171fc32b081a3dc43ae7035cc55f4
swift1719 <ayushpawar1501@gmail.com>
2022-08-27 19:58:27 +0530

    handled io at program entrance

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
 ﻿
+namespace MerchantsGuideToGalaxy
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            Console.Write("Enter no of lines : ");
+            int noOfInformationLines = int.Parse(Console.ReadLine());
+
+            string[] informationLines = new string[noOfInformationLines];
+            for (int i = 0; i < noOfInformationLines; i++)
+            {
+                informationLines[i] = Console.ReadLine();
+            }
+            ProcessingScript service = new ProcessingScript();
+            
+            foreach(var response in service.PlayScript(informationLines))
+            {
+                Console.WriteLine(response);
+            }
+        }
+    }
+}
