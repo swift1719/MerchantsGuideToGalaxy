019ca1ee358acdfbab18fdffdca5c0e2784cae2c
swift1719 <ayushpawar1501@gmail.com>
2022-08-27 19:48:49 +0530

    handling of statements and queries using grammar

---
+++ Language.cs
@@ -0,0 +1,34 @@
+﻿using MerchantsGuideToGalaxy.Grammar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantsGuideToGalaxy
+{
+    public class Language
+    {
+        private readonly List<IGrammar> _grammers = new List<IGrammar>
+        {
+            new NumberAliasStatement_Grammar(),
+            new NumberConversionQuery_Grammar(),
+            new ProductValueStatement_Grammar(),
+            new ProductPriceQuery_Grammar()
+        };
+        public bool TryParse(string text, out ISentence sentence)
+        {
+            sentence = null;
+
+            foreach (var grammer in _grammers)
+            {
+                if(grammer.TryParse(text, out sentence) == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
