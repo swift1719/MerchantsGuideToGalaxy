3b7213ce2b7553e6e88d2973273ef6365568901c
swift1719 <ayushpawar1501@gmail.com>
2022-08-27 16:56:09 +0530

    created product class

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace MerchantsGuideToGalaxy
+{
+    public class Product
+    {
+
+        public Product(string name, string price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; }
+        public string Price { get; }
+    }
+}
